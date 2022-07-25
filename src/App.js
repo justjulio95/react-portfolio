6d0ccc88169fbbf43e50fc82c3bca365692d9095
Julio.S <just.julio95@gmail.com>
2022-07-25 00:26:09 -0400

    got the bare bones going for portfolio section

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Header from './components/Header';
 import ContactForm from './components/Contact';
 import About from './components/About';
+import Projects from './components/Portfolio';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+  
+  // const [contactSelected, setContactSelected] = useState(false)
+
   return (
     <div>
       <Header></Header>
 
       <main>
         <ContactForm></ContactForm>
+        <Projects></Projects>
         <About></About>
       </main>
 
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import MusicFind from "../../assets/imgs/musicfind.png"
+
+function Projects() {
+  return(
+    <div className="d-flex flex-wrap justify-content-center">
+      <div className="card border-secondary mx-3 my-4" style={{width:`25rem`}}>
+        <h2 className="card-header">Project Title</h2>
+        <a href="https://www.github.com" target="_blank">
+          <img className="card-img-top" src={MusicFind} alt="test"/>
+        </a>
+      </div>
+      <div className="card border-secondary mx-3 my-4" style={{width:`25rem`}}>
+        <h2 className="card-header">Project Title</h2>
+        <a href="https://www.github.com" target="_blank">
+          <img className="card-img-top" src={MusicFind} alt="test"/>
+        </a>
+      </div>
+      <div className="card border-secondary mx-3 my-4" style={{width:`25rem`}}>
+        <h2 className="card-header">Project Title</h2>
+        <a href="https://www.github.com" target="_blank">
+          <img className="card-img-top" src={MusicFind} alt="test"/>
+        </a>
+      </div>
+      <div className="card border-secondary mx-3 my-4" style={{width:`25rem`}}>
+        <h2 className="card-header">Project Title</h2>
+        <a href="https://www.github.com" target="_blank">
+          <img className="card-img-top" src={MusicFind} alt="test"/>
+        </a>
+      </div>
+      <div className="card border-secondary mx-3 my-4" style={{width:`25rem`}}>
+        <h2 className="card-header">Project Title</h2>
+        <a href="https://www.github.com" target="_blank">
+          <img className="card-img-top" src={MusicFind} alt="test"/>
+        </a>
+      </div>
+      <div className="card border-secondary mx-3 my-4" style={{width:`25rem`}}>
+        <h2 className="card-header">Project Title</h2>
+        <a href="https://www.github.com" target="_blank">
+          <img className="card-img-top" src={MusicFind} alt="test"/>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Projects;
