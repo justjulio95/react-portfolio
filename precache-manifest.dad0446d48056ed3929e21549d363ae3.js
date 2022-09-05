9223b6671843816436ec9ddcc875c825e97121d8
Julio.S <just.julio95@gmail.com>
2022-09-05 12:12:02 -0400

    Updates

---
+++ precache-manifest.dad0446d48056ed3929e21549d363ae3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "67c0081f59c022593ba6",
+    "url": "/react-portfolio/static/css/main.9bafa338.chunk.css"
+  },
+  {
+    "revision": "67c0081f59c022593ba6",
+    "url": "/react-portfolio/static/js/main.67c0081f.chunk.js"
+  },
+  {
+    "revision": "6027f9d15d8148605ea1",
+    "url": "/react-portfolio/static/js/1.6027f9d1.chunk.js"
+  },
+  {
+    "revision": "30a2ac5730739649e66a",
+    "url": "/react-portfolio/static/js/2.30a2ac57.chunk.js"
+  },
+  {
+    "revision": "7f0ec30de9c4cfbb8f27",
+    "url": "/react-portfolio/static/js/runtime~main.7f0ec30d.js"
+  },
+  {
+    "revision": "f9c27ef22780f4facf44f950368e6bf2",
+    "url": "/react-portfolio/static/media/headshot.f9c27ef2.jpg"
+  },
+  {
+    "revision": "c73b44061f0727f7347e184768525bbf",
+    "url": "/react-portfolio/static/media/homebrew.c73b4406.png"
+  },
+  {
+    "revision": "21ec309272e56a2d6de6ca8c256763f5",
+    "url": "/react-portfolio/static/media/readITready.21ec3092.png"
+  },
+  {
+    "revision": "6dbdd538ef9450da88a745b96bee20db",
+    "url": "/react-portfolio/static/media/budget_tracker.6dbdd538.png"
+  },
+  {
+    "revision": "5b2bd83fe13d5a118ad52ce4b4a18637",
+    "url": "/react-portfolio/static/media/musicfind.5b2bd83f.png"
+  },
+  {
+    "revision": "5194945d1c009611b851f437e64d0d03",
+    "url": "/react-portfolio/static/media/weather-dashboard.5194945d.png"
+  },
+  {
+    "revision": "55059455c932ab39538365442b6d19e8",
+    "url": "/react-portfolio/static/media/resume.55059455.pdf"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/react-portfolio/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "64679351069ec79b65ce22fee6023949",
+    "url": "/react-portfolio/index.html"
+  }
+];
